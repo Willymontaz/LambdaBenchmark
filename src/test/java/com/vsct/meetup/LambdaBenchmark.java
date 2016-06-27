c0bbc332f9923e3e0dbcd39ea84aae7f9f587ad0
william_montaz <willymontaz@gmail.com>
2016-06-27 16:03:26 +0200

    Implem tests

--- src/test/java/com/vsct/meetup/LambdaBenchmark.java
+++ src/test/java/com/vsct/meetup/LambdaBenchmark.java
@@ -1,12 +1,18 @@
 package com.vsct.meetup;
 
 import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.profile.CompilerProfiler;
+import org.openjdk.jmh.profile.GCProfiler;
+import org.openjdk.jmh.profile.StackProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 /**
  * ~  Copyright (C) 2016 VSCT
@@ -30,17 +36,79 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(1)
+@Fork(value = 1, jvmArgs = {"-Xms256m","-Xmx256m"})
 @State(Scope.Benchmark)
 public class LambdaBenchmark {
+
+    String capturable = "concat";
+
+    String applyFunction(String word) {
+        return concatenate(word);
+    }
+
+    String concatenate(String word) {
+        return word + " concat";
+    }
+
+    String applyLambda(Function<String, String> lambda, String word) {
+        return lambda.apply(word);
+    }
+
+    String applyInner(Inner inner, String word){
+        return inner.apply(word);
+    }
+
+    static interface Inner {
+        String apply(String word);
+    }
 
     @Benchmark
     public void baseline() {
     }
 
+    @Benchmark
+    public String method_call() {
+        return applyFunction("word");
+    }
+
+    @Benchmark
+    public String inner_class_without_capture() {
+        return applyInner(new Inner() {
+            @Override
+            public String apply(String s) {
+                return s + "concat";
+            }
+        }, "word");
+    }
+
+    @Benchmark
+    public String lambda_without_capture() {
+        return applyLambda(s -> {
+            return s + "concat";
+        }, "word");
+    }
+
+    @Benchmark
+    public String inner_class_capturing() {
+        return applyInner(new Inner() {
+            @Override
+            public String apply(String s) {
+                return s + capturable;
+            }
+        }, "word");
+    }
+
+    @Benchmark
+    public String lambda_capturing() {
+        return applyLambda(s -> {
+            return s + capturable;
+        }, "word");
+    }
+
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(".*" + LambdaBenchmark.class.getSimpleName() + ".*")
+                .addProfiler(GCProfiler.class)
                 .build();
 
         new Runner(opt).run();
